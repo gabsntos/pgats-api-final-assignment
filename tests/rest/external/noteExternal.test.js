6e70f4cfbadad7e24d7ea111ac9d5dd7627d055b
gabriel <gabriel545.545@gmail.com>
2025-09-16 22:30:23 -0300

    building rest external tests

---
+++ tests/rest/external/noteExternal.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest')
+const { expect } = require('chai')
+
+const auth = require('../../authSetup')
+
+describe('noteExternal', () => {
+
+    const deleteNoteWithError = require('../fixture/request/deleteNoteWithError.json')
+
+    describe('DELETE /notes', () => {
+
+        deleteNoteWithError.forEach(test => {
+            it(`${test.name}`, async () => {
+                const response = await request('http://localhost:3000')
+                    .delete(`/notes/${test.parameter}`)
+                    .set('Authorization', `Bearer ${auth.getToken()}`)
+
+                expect(response.status).to.equal(404)
+                expect(response.body).to.have.property('error', test.expectedMessage)
+            })
+        })
+
+    })
+})
---
+++ tests/rest/external/taskExternal.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest')
+const { expect } = require('chai')
+
+const auth = require('../../authSetup')
+
+describe('taskExternal', () => {
+
+    describe('GET /tasks', () => {
+
+        it('Should return 200 get tasks', async () => {
+            const response = await request('http://localhost:3000')
+                .get('/tasks')
+                .set('Authorization', `Bearer ${auth.getToken()}`)
+
+            expect(response.status).to.equal(200)
+            expect(response.body[0]).to.have.property('id', 1)
+            expect(response.body[0]).to.have.property('title', 'Buy groceries')
+        })
+    })
+})
